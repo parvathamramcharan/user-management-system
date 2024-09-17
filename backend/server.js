73b2715bc0a2bc638382799d1ea2caaeaac79b6a
parvathamramcharan <parvathamramcharan7@gmail.com>
2024-09-17 21:28:28 +0530

    Updated file structure

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,18 @@ const user = require('./routes/route')
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    // origin: 'https://your-frontend-domain.com', // Replace with your actual frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  };
+  
+  app.use(cors(corsOptions));
+//   app.use(cors());
 app.get('/',(req,res)=>{
     res.send("hello");
 })
-const MONGO_URL="mongodb://localhost:27017/PRACTICE"
+const MONGO_URL="mongodb+srv://parvathamramcharan7:Msdhoni133@cluster0.f3nc6.mongodb.net/usermanage?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose.connect(MONGO_URL)
 .then( ()=> console.log("mongodb connected"))
