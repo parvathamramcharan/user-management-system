f8ac54e3c3e9c5eda72027509c44ef43be85b710
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2024-09-17 22:34:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const cors= require('cors')
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const mongoose = require('mongoose')
-// const User = require('./Model/user')
-const user = require('./routes/route')
+const mongoose = require('mongoose');
+const user = require('./routes/route');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: 'https://user-management-system-front.vercel.app'  // Allow only this origin
-}));
-  
-  app.use(cors(corsOptions));
-//   app.use(cors());
-app.get('/',(req,res)=>{
-    res.send("hello");
-})
+
+// Define CORS options
+const corsOptions = {
+  origin: process.env.FRONTEND_URL
+};
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+  res.send("hello");
+});
+
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connect(MONGO_URL)
-.then( ()=> console.log("mongodb connected"))
-.catch(err=> console.log(err))
-app.use('/student',user);
-const PORT=4000
-app.listen(PORT,()=>{
-        console.log(`server is running on port :${PORT}`)
-})
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.use('/student', user);
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port :${PORT}`);
+});
