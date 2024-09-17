e0c4215723af125aaa8a883a23e075f33c2e1003
Parvatham Ram Charan <parvathamramcharan7@gmail.com>
2024-09-17 22:04:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,8 @@ const user = require('./routes/route')
 const app = express();
 app.use(express.json());
 const corsOptions = {
-     origin: ["https://user-management-system-frontend-beta.vercel.app" ], // Replace with your actual frontend domain
-    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
-    allowedHeaders: 'Content-Type, Authorization',
+     origin: ['https://user-management-system-frontend-beta.vercel.app'], // Replace with your actual frontend domain
+    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"]
   };
   
   app.use(cors(corsOptions));
